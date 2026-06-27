41f421c1d7dfe9bab0fb6a448034d5c5970f2e3b
Laura Souza <163550002+LauraMilly@users.noreply.github.com>
2026-06-27 11:16:31 -0300

    corrige o arquivo: adiciona interface e metodos

--- src/main/java/com/mv/clinica_backend/repository/PacienteRepository.java
+++ src/main/java/com/mv/clinica_backend/repository/PacienteRepository.java
@@ -1,4 +1,13 @@
 package com.mv.clinica_backend.repository;
 
-public class PacienteRepository {
+import com.mv.clinica_backend.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+
+    Paciente findByCpf(String cpf);
+
+    Paciente findByEmail(String email);
 }
