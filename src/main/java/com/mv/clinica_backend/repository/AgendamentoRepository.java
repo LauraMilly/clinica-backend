70185f8525edcd51c51e397f7a5fda39111efdcd
Laura Souza <163550002+LauraMilly@users.noreply.github.com>
2026-06-27 11:49:32 -0300

    Corrige AgendamentoRepository: adiciona interface e métodos customizados

--- src/main/java/com/mv/clinica_backend/repository/AgendamentoRepository.java
+++ src/main/java/com/mv/clinica_backend/repository/AgendamentoRepository.java
@@ -1,4 +1,22 @@
 package com.mv.clinica_backend.repository;
 
-public interface AgendamentoRepository {
+import com.mv.clinica_backend.entity.Agendamento;
+import com.mv.clinica_backend.entity.Paciente;
+import com.mv.clinica_backend.entity.Profissional;
+import com.mv.clinica_backend.entity.StatusAgendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+    List<Agendamento> findByPaciente(Paciente paciente);
+
+    List<Agendamento> findByProfissional(Profissional profissional);
+
+    List<Agendamento> findByStatus(StatusAgendamento status);
+
+    boolean existsByProfissionalAndDataHora(Profissional profissional, LocalDateTime dataHora);
 }
