d1852f1e37f26cbb8d093e44235f243106ab2f9d
Laura Souza <163550002+LauraMilly@users.noreply.github.com>
2026-06-27 11:33:27 -0300

    adiciona repository layer para entity profissional

---
+++ src/main/java/com/mv/clinica_backend/repository/ProfissionalRepository.java
@@ -0,0 +1,14 @@
+package com.mv.clinica_backend.repository;
+
+import com.mv.clinica_backend.entity.Profissional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProfissionalRepository extends JpaRepository <Profissional, Long>{
+
+    Profissional findByCrm(String crm);
+
+    List<Profissional> findByEspecialidades(String especialidade);
+}
