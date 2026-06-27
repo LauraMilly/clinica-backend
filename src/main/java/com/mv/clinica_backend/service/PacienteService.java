c1dbfaaa3108fe6cce84839601d490c617ccf4d4
Laura Souza <163550002+LauraMilly@users.noreply.github.com>
2026-06-27 12:52:51 -0300

    correção do arquivo

--- src/main/java/com/mv/clinica_backend/service/PacienteService.java
+++ src/main/java/com/mv/clinica_backend/service/PacienteService.java
@@ -1,4 +1,33 @@
 package com.mv.clinica_backend.service;
 
+import com.mv.clinica_backend.entity.Paciente;
+import com.mv.clinica_backend.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public Paciente cadastrar(Paciente paciente) {
+
+        Paciente pacienteExistente = pacienteRepository.findByCpf(paciente.getCpf());
+        if (pacienteExistente != null) {
+            throw new RuntimeException("CPF já cadastrado!");
+        }
+
+        return pacienteRepository.save(paciente);
+    }
+
+    public List<Paciente> listarTodos() {
+        return pacienteRepository.findAll();
+    }
+
+    public Paciente buscarPorId(Long id) {
+        return pacienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Paciente não encontrado!"));
+    }
 }
